7aa0b052745f7c07f4f66cb7e7d26a44c8d2ad02
Karas358 <mfundo.falteni>
2020-09-17 10:43:55 +0200

    master to origin

--- leet_RemoveDuplicates/RemoveDuplicates.java
+++ leet_RemoveDuplicates/RemoveDuplicates.java
@@ -12,22 +12,22 @@ public class RemoveDuplicates {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
         int j = removeDuplicatesInArray(nums);
     }
-
-    public static int removeDuplicatesInArray(int[] nums){
-        int temp = 0;
-        int n = nums.length;
-        int count = 1;
-        for(int x = 0; x < n - 1;x++){
-            for(int z = x + 1; x < n - 1 ;z++){
-                if(nums[x] != nums[z]){
-                    count++;
-                    temp = nums[x + 1];
-                    nums[x + 1] = nums[z];
-                    nums[z] = temp;
-                    x = z;
-                }
-            }
-        }
-        return count;
-    }
+    public static int removeDuplicatesInArray(int[] nums) {
+         int temp = 0;
+         int n = nums.length - 1;
+         int count = 1;
+         for(int x = 0; x < n;x++){
+             for(int z = x + 1; z < n + 1 ;z++){
+                 if(nums[x] != nums[z]){
+                     count++;
+                     temp = nums[x + 1];
+                     nums[x + 1] = nums[z];
+                     nums[z] = temp;
+                     x++;
+                 }
+                 if(z == n) return count;
+             }
+         }
+         return count; 
+     }
 }
