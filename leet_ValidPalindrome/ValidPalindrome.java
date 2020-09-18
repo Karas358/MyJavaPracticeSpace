becad6513b30f2de94913450450518465f8d25db
Karas358 <mfundo.falteni>
2020-09-18 19:22:34 +0200

    master to origin

---
+++ leet_ValidPalindrome/ValidPalindrome.java
@@ -0,0 +1,55 @@
+package leet_ValidPalindrome;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidPalindrome{
+    /**
+     * Developer - Mfundo Falteni
+     * @param {string} s
+     * @return {boolean}
+    */
+    public static void main(String[] args) {
+        /* String s = "A man, a plan, a canal: Panama";
+        
+        String s = "aa";
+        String s = "";
+        String s = ".,";*/
+        String s = "race a car";
+        boolean check = isPalindrome(s);
+    }
+    public static boolean isPalindrome(String s){
+        
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]","");
+        if(s.length() == 1 || s == null || s.length() == 0)  return true;
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while(left < right){
+            if(Character.toLowerCase(chars[left]) != Character.toLowerCase(chars[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+
+
+    /* public static boolean isPalindrome(String s){
+        if(s == null) return false;
+        String regex = ":*,*\s*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        s = matcher.replaceAll("");
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while(left != right){
+            if(Character.toLowerCase(chars[left]) != Character.toLowerCase(chars[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    } */
+}
